f05630927659486f46d55cb4ee6b8493c5871c7f
Jatin_Mehta <mehtajatin045@gmail.com>
2025-04-29 19:02:24 +0530

    Add route

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,6 @@ import Views from '@/views'
 import appConfig from './configs/app.config'
 import './locales'
 import Home from './views/Home'
-import HeroSection from './views/Home/components/HeroSection'
-import FeaturesGrid from './views/Home/components/FeaturesGrid'
-import ClaimLandingSection from './views/Home/components/ClaimLandingSection'
-import FullPageSections from './views/Home/components/InfoSection'
-import ContactForm from './views/Home/components/ContactForm'
-import FAQ from './views/Home/components/HomeFAQ'
-import MainFooter from './views/Home/components/MainFooter'
 
 if (appConfig.enableMock) {
     import('./mock')
